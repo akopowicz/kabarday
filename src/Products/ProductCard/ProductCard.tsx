40b633e4a59e3a580663287d0d36daa35baeaf9e
akopowicz <anna.kopowicz@gmail.com>
2024-01-02 20:04:21 +0100

    ProductCard

--- src/Products/ProductCard/ProductCard.tsx
+++ src/Products/ProductCard/ProductCard.tsx
@@ -7,11 +7,14 @@ import { useEffect, useState } from "react";
 import { SimilarProducts } from "../SimilarProducts/SimilarProducts";
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+// import ArrowCircleRightTwoToneIcon from '@mui/icons-material/ArrowCircleRightTwoTone';
 
 export default function ProductCard() {
     const location = useLocation();
     const id = location.search.split("=")[1]
-
+    const [caroselPhotosChange, setCaroselPhotosChange] = useState(3);
 
     // const queryParameters = new URLSearchParams(location.search)
 
@@ -68,8 +71,23 @@ export default function ProductCard() {
         setMainPhoto(photoSrc)
     }
 
+    const removeMove = () => {
+        if (caroselPhotosChange > 3) {
+            setCaroselPhotosChange(prev=>prev-1)
+        }
 
-    console.log(product.photos)
+        console.log(caroselPhotosChange)
+    }
+
+    const addMove = () => {
+    //    if (product.photos.length-1>caroselPhotosChange) {
+        setCaroselPhotosChange(prev=>prev+1)
+        console.log(caroselPhotosChange)
+    //    }
+    }
+
+
+    console.log(product.photos.length)
     return (
         <div className={style.productCard}>
             <div className={style.productWrapper}>
@@ -99,8 +117,10 @@ export default function ProductCard() {
 
 
                                 </Slider>
-                                <ButtonBack>Back</ButtonBack>
-                                <ButtonNext>Next</ButtonNext>
+                                <ButtonBack className={`${style.arrowCarusel} ${style.arrowLeft} ${caroselPhotosChange === 3? style.disabled : ''}`}>
+                                    <ArrowCircleLeftIcon className={style.arrowIcon} onClick={removeMove}/>
+                                    </ButtonBack>
+                                <ButtonNext className={`${style.arrowCarusel} ${style.arrowRight} ${product.photos.length-1 > caroselPhotosChange? "": style.disabled}`}><ArrowCircleRightIcon className={style.arrowIcon} onClick={addMove}/></ButtonNext>
                             </CarouselProvider>
                             {/* <div className={style.smallPhotosWrapper}>
                                 <div className={style.smallPhotos}>
@@ -134,9 +154,19 @@ export default function ProductCard() {
                                     <h3 className={style.description}>Opis:</h3>
                                     <p className={style.descriptionText}>{product.description}</p>
                                 </div>
-                                <div className={style.delivery}><h3>Sposób skladania zmównienia</h3>
-                                    <p>Zamówienie składa się.... </p>
+                                <div>
+                                    <h3 className={style.colors}>Dostępne kolory:</h3>
+                                    <p className={style.colorsText}>Kolory w których występuje dana włóczka przedstawione są na zdjęciu. W zależności od parametrów wyświetlacza kolory mogą różnić się od faktycznego ich wyglądu. Dostępność kolorów może różnić się od faktycznej dostępności w danym momencie.</p>
                                 </div>
+                                <div className={style.delivery}><h3>Sposób skladania zmównienia:</h3>
+                                    <p className={style.deliveryText}>W celu złożenia zamówienia napisz do nas wiadomość na adres mailowy: <a className={style.mail} href="mailto:info.kabarday@gmail.com">info.kabarday@gmail.com</a></p>
+                                </div>
+
+                                <div className={style.time}>
+                                    <h3>Czas realizacji zamówienia:</h3>
+                                    <p>Po zaksięgowaniu wpłaty za zamówienie czas realizacji wynosi od 5 do 21 dni</p>
+                                </div>
+                                <a href={`mailto:info.kabarday@gmail.com?subject=${product.name}?body=tu wpisz tekst`} className={style.order}>Zamów</a>
                             </div>
 
                         </div>

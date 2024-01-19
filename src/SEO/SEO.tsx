91228ce55bc221721dbfd0e24c2231247733d19c
akopowicz <anna.kopowicz@gmail.com>
2024-01-19 16:59:22 +0100

    helmet

--- src/SEO/SEO.tsx
+++ src/SEO/SEO.tsx
@@ -6,17 +6,28 @@ return (
 <title>{title}</title>
 <meta name='description' content={description} />
 <meta property="image" content={photo}></meta>
+<meta property="image:type" content="image/png" />
+<meta property="image:width" content="240" />
+<meta property="image:height" content="240" />
+<meta property="image:alt" content="logo" />
 { /* End standard metadata tags */ }
 { /* Facebook tags */ }
 <meta property="og:title" content={title} />
 <meta property="og:description" content={description} />
 <meta property="og:image" content={photo}></meta>
+<meta property="og:image:type" content="image/png" />
+<meta property="og:image:width" content="240" />
+<meta property="og:image:height" content="240" />
+<meta property="og:image:alt" content="Display Picture" />
 { /* End Facebook tags */ }
 { /* Twitter tags */ }
 <meta name="twitter:creator" content={name} />
 <meta name="twitter:title" content={title} />
 <meta name="twitter:description" content={description} />
 <meta name="twitter:image" content={photo} />
+<meta property="twitter:image:width" content="240" />
+<meta property="twitter:image:height" content="240" />
+<meta property="twitter:image:alt" content="logo" />
 { /* End Twitter tags */ }
 </Helmet>
 )

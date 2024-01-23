fae5bd5fe2c372ac2068ad8f971232e5b6665b92
akopowicz <anna.kopowicz@gmail.com>
2024-01-23 22:00:06 +0100

    navigation

--- src/Products/ProductCard/PathInformation/PathInformation.tsx
+++ src/Products/ProductCard/PathInformation/PathInformation.tsx
@@ -7,6 +7,6 @@ import {ROUTES} from "../../../constants";
 export const PathInformation = ({product}: {product: productType}) => {
     console.log(product)
     return (
-        <div className={style.pathWrapper}><Link to="/" className={style.pathLink}>Strona Główna</Link><KeyboardArrowRightIcon className={style.arrow} /><Link to={ROUTES.products}>Produkty</Link><KeyboardArrowRightIcon className={style.arrow} /><Link to={`/products/${product.product_type?.type_name}`} state={product.product_type?.id} className={style.pathLink}>{product.product_type?.type_name}</Link><KeyboardArrowRightIcon className={style.arrow} /><span className={style.pathLink}>{product.name}</span></div>
+        <div className={style.pathWrapper}><Link to="/" className={style.pathLink}>Strona Główna</Link><KeyboardArrowRightIcon className={style.arrow} /><Link to={ROUTES.products}>Produkty</Link><KeyboardArrowRightIcon className={style.arrow} /><Link to={`/products/${product.product_type?.type_name}?typeId=${product.product_type?.id}`} state={product.product_type?.id} className={style.pathLink}>{product.product_type?.type_name}</Link><KeyboardArrowRightIcon className={style.arrow} /><span className={style.pathLink}>{product.name}</span></div>
     )
 }
